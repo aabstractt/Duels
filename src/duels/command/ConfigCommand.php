5c94861cc0c3c224e98bd92d0c986900bee3e29d
Xavier <xavier04.martinez@gmail.com>
2021-03-20 23:36:07 -0600

    Duels sub command added (FendiPvP)

--- src/duels/command/ConfigCommand.php
+++ src/duels/command/ConfigCommand.php
@@ -6,6 +6,7 @@ namespace duels\command;
 
 use duels\api\Command;
 use duels\command\subcommand\CreateCommand;
+use duels\command\subcommand\DuelsCommand;
 use duels\command\subcommand\LobbyItemsCommand;
 use duels\command\subcommand\SpawnCommand;
 use duels\command\subcommand\TopCommand;
@@ -22,7 +23,8 @@ class ConfigCommand extends Command {
             new CreateCommand('create', 'config.command.create'),
             new SpawnCommand('spawn', 'Set a spawn'),
             new LobbyItemsCommand('lobbyitems', 'config.command.lobbyitems'),
-            new TopCommand('top', 'config.command.top')
+            new TopCommand('top', 'config.command.top'),
+            new DuelsCommand('duels')
         );
     }
 }
--- src/duels/command/subcommand/DuelsCommand.php
+++ src/duels/command/subcommand/DuelsCommand.php
@@ -1,7 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace duels\command\subcommand;
 
-class DuelsCommand {
+use duels\api\PlayerSubCommand;
+use duels\session\Session;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
 
+class DuelsCommand extends PlayerSubCommand {
+
+    /**
+     * @param Session $session
+     * @param array $args
+     */
+    public function onRun(Session $session, array $args): void {
+        $data = [
+            'type' => 'form',
+            'title' => TextFormat::BLUE . TextFormat::BOLD . 'Play Duels',
+            'content' => 'Select which mode you would like to play.',
+            'buttons' => [
+                ['text' => 'Ranked Duels'],
+                ['text' => 'Unranked Duels'],
+                ['text' => 'Request Duels'],
+                ['text' => 'Spec Duels']
+            ]
+        ];
+
+        $session->sendForm(function (Session $session, ?int $data): void {
+            if ($data === null) return;
+
+            if ($data <= 1) {
+                Server::getInstance()->dispatchCommand($session->getGeneralPlayer(), 'queue list ' . ($data == 0 ? 'ranked' : 'unranked'));
+            } else {
+                $session->sendMessage('Soon');
+            }
+        }, $data);
+    }
 }
